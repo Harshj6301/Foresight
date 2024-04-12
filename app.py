63caae20cfda5076d833a53d75e86cd5d0223132
Harshvardhan Jadhav <jadhavharshvardhan6301@gmail.com>
2024-04-12 12:49:14 +0530

    Update app.py
    changed width of logo

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def main():
     
     # Add your logo/image
     logo = 'Assets/Logos/DecisionDeltaLogobyDesigner (1).png'
-    st.image(logo, caption='Decision Delta', use_column_width=True)
+    st.image(logo, caption='Decision Delta', width=200)
 
     st.subheader('Provide Data')
 
