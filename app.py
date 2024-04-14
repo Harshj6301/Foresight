a9855e7e93525e6dbe40d4a6abd93a52aaa5b08a
Harshvardhan Jadhav <jadhavharshvardhan6301@gmail.com>
2024-04-14 20:53:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ def main():
         with col2:
             feature_2 = st.selectbox("Select Y", data_features)"""
         # line chart
-        plot(data, feature_1[0], feature_2[1])
+        plot(data, x_feature, y_feature)
         
         # Plotly chart
         #fig = px.line(data, x='Datetime', y='Close')
